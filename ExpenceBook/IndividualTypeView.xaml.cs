0d91938caf97308aef8d9a8b2af83abf3f6139a0
annams1 <annams1@hawkmail.newpaltz.edu>
2015-05-14 10:00:36 -0400

    A

---
+++ ExpenceBook/IndividualTypeView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MicroMVVM.Expence.Daily;
+using System.Configuration;
+
+namespace ExpenceBook
+{
+    /// <summary>
+    /// Interaction logic for IndividualTypeView.xaml
+    /// </summary>
+    public partial class IndividualTypeView : UserControl
+    {
+        public IndividualTypeView()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string transactionType = ComboBox1.Text;
+
+            List<DailyExpence> lstDailyExpence = new List<DailyExpence>();
+
+            string loginId = ConfigurationManager.AppSettings["loginUserId"];
+
+            lstDailyExpence = DailyExpence.IndividualSort(loginId, transactionType);
+
+            lvDailyExpence.DataContext = lstDailyExpence;
+        }
+
+    }
+}
